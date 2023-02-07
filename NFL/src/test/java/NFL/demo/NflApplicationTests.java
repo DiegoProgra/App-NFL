2f845bbf2a404611ca32d19091846e5bf59794a2
DiegoProgra <123037126+DiegoProgra@users.noreply.github.com>
2023-02-07 15:42:37 -0600

    Add files via upload

---
+++ NFL/src/test/java/NFL/demo/NflApplicationTests.java
@@ -0,0 +1,13 @@
+package NFL.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NflApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
